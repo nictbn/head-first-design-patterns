ccf3929b3a6a22e532cb704b687cbb59f5ebddbf
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-05 22:51:51 +0300

    Adds code for the Decorator Pattern.

---
+++ decorator/src/condiments/SteamedMilk.java
@@ -0,0 +1,17 @@
+package condiments;
+
+import beverages.Beverage;
+
+public class SteamedMilk extends CondimentDecorator {
+    Beverage beverage;
+
+    public SteamedMilk(Beverage beverage) {this.beverage = beverage;}
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Steamed Milk";
+    }
+
+    @Override
+    public double cost() {return 0.10 + beverage.cost();}
+}
